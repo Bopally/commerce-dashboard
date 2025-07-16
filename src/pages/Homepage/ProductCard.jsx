8f35bd225b0ada286f1f001fbd7aac6adeed9e0e
Estelle Ballot <estelle.ballot@qonto.com>
2025-07-16 08:40:44 +0200

    Add route details for each product

--- src/pages/Homepage/ProductCard.jsx
+++ src/pages/Homepage/ProductCard.jsx
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 
 function ProductCard({ product }) {
   return (
-    <div className="product-card" key={product.id}>
-      <div className="image-hover-container">
-        <img
-          src={product.thumbnail}
-          alt={product.title}
-          className="product-image"
-        />
-        <div className="description-hover">
-          <p>{product.description}</p>
+    <Link to={`/products/${product.id}`} className="product-card-link">
+      <div className="product-card" key={product.id}>
+        <div className="image-hover-container">
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="product-image"
+          />
+          <div className="description-hover">
+            <p>{product.description}</p>
+          </div>
         </div>
+        <h2 className="product-title">{product.title}</h2>
+        <p className="product-price">
+          <strong>{product.price} €</strong>
+        </p>
+        <p className="product-category">{product.category}</p>
       </div>
-      <Link to={`/product/${product.id}`}></Link>
-      <h2 className="product-title">{product.title}</h2>
-      <p className="product-price">
-        <strong>{product.price} €</strong>
-      </p>
-      <p className="product-rating">Rating: {product.rating} / 5</p>
-      <p className="product-category">{product.category}</p>
-    </div>
+    </Link>
   )
 }
 
