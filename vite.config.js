9b2019ea8507fb7e68a6e69fb329902a4129c041
Estelle Ballot <estelle.ballot@qonto.com>
2025-07-18 11:29:17 +0200

    Update congif for route

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  // Base URL seulement pour la production (GitHub Pages)
-  base: command === 'build' ? '/commerce-dashboard/' : '/',
+  base: '/commerce-dashboard',
   build: {
     outDir: 'docs',
   },
-}))
+})
