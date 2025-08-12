3a5f1894a857fa6c8188630ead32c6d2bdb0c032
Estelle Ballot <estelle.ballot@qonto.com>
2025-08-12 16:17:46 +0200

    Update api service for loginUser

--- src/services/api.service.jsx
+++ src/services/api.service.jsx
@@ -1,15 +1,38 @@
-const BASE_URL = 'https://dummyjson.com';
+const BASE_URL = 'https://dummyjson.com'
 
 export const fetchData = async (endpoint) => {
   try {
-    const response = await fetch(`${BASE_URL}/${endpoint}`);
+    const response = await fetch(`${BASE_URL}/${endpoint}`)
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-    return await response.json();
+    return await response.json()
   } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
+    console.error('Error fetching data:', error)
+    throw error
   }
-};
+}
 
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      })
+    })
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    
+    return await response.json()
+  } catch (error) {
+    console.error('Error during login:', error)
+    throw error
+  }
+}
