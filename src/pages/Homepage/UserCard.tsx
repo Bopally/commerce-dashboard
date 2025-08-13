c7062d024e32c24d652d64eed0e897307b5c2302
Estelle Ballot <estelle.ballot@qonto.com>
2025-08-13 15:24:52 +0200

    Add Typescript to card and users

--- src/pages/Homepage/UserCard.tsx
+++ src/pages/Homepage/UserCard.tsx
@@ -1,7 +1,83 @@
 import './UserCard.css'
 import { Link } from 'react-router-dom'
-import { fetchData } from '../../services/api.service'
+import { fetchData } from '../../services/api.service.ts'
 import { useState, useEffect } from 'react'
+import type { Cart } from '../../types/cart.d.ts'
+
+interface User {
+  id: number
+  firstName: string
+  lastName: string
+  maidenName: string
+  age: number
+  gender: string
+  email: string
+  phone: string
+  username: string
+  password: string
+  birthDate: string
+  image: string
+  bloodGroup: string
+  height: number
+  weight: number
+  eyeColor: string
+  hair: {
+    color: string
+    type: string
+  }
+  ip: string
+  address: {
+    address: string
+    city: string
+    state: string
+    stateCode: string
+    postalCode: string
+    coordinates: {
+      lat: number
+      lng: number
+    }
+    country: string
+  }
+  macAddress: string
+  university: string
+  bank: {
+    cardExpire: string
+    cardNumber: string
+    cardType: string
+    currency: string
+    iban: string
+  }
+  company: {
+    department: string
+    name: string
+    title: string
+    address: {
+      address: string
+      city: string
+      state: string
+      stateCode: string
+      postalCode: string
+      coordinates: {
+        lat: number
+        lng: number
+      }
+      country: string
+    }
+  }
+  ein: string
+  ssn: string
+  userAgent: string
+  crypto: {
+    coin: string
+    wallet: string
+    network: string
+  }
+  role: string
+}
+
+interface UserCardProps {
+  user: User
+}
 
 /**
  * @typedef {import('../../types/interfaces.js').User} User
@@ -15,15 +91,15 @@ import { useState, useEffect } from 'react'
  * @param {UserHandlers} props.handlers - Handler functions from parent component
  * @returns {JSX.Element}
  */
-function UserCard({ user }) {
-  const [userCarts, setUserCarts] = useState([])
+function UserCard({ user }: UserCardProps) {
+  const [userCarts, setUserCarts] = useState<Cart[]>([])
   const [userCartsLoading, setUserCartsLoading] = useState(true)
 
   useEffect(() => {
     const loadUserCarts = async () => {
       try {
         const data = await fetchData(`carts/user/${user.id}`)
-        setUserCarts(data.carts)
+        setUserCarts(data.carts as Cart[])
       } catch (err) {
         console.error('Failed to load user carts:', err.message)
       } finally {
